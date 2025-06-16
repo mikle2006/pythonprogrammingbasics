db5598af010bc0451371df27acdef19472f983c4
mikle2006 <mungaimichael629@gmail.com>
2025-06-16 04:35:24 -0400

    final commit

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+
+number =45 #Integer
+second =34.98 #float
+greeting ="Good Morning" #String
+isPythonInteresting = True #Boolean
+
+
+#Data Structures
+fruits =["apple", "mango", "cherry"] # List
+cars =("Jeep","BMW", "Vitz,","Toyota")# Tuple
+countries ={"Kenya","Canada","Maldives","Canada"}#Set
+student ={
+    "firstname":"Michael",
+    "lastname":"Mungai",
+    "age":21,
+    "nationality":"Kenyan"
+}# Dictionary -Key-value pair
+
+print(number)
+print(isPythonInteresting)
+print(fruits)
+print(cars)
+print(countries)
+print(student["nationality"])
+
+
+#Typecasting -Converting one data to another
+print(float(number))
+print(int(second))
---
+++ introduction.py
@@ -0,0 +1,7 @@
+#Print of a method used to output a value
+
+
+print("Welcome to python programming ")
+print("Glory")
+print(100)
+print(10 + 20)
